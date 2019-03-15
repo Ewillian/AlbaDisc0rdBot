9da79257875df9d4d377a35671be8c97903dbd4a
Ewillian <guillaume.lecoq@ynov.com>
2019-03-15 15:52:08 +0100

    added connection

---
+++ AlbaBotDiscord/Core/Commands/HelloThere.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+
+using Discord;
+using Discord.Commands;
+
+namespace AlbaBotDiscord.Core.Commands
+{
+    class HelloThere : ModuleBase<SocketCommandContext>
+    {
+        [Command("Hello"), Alias("SalutATous"), Summary("Répond de manière cultisime.")]
+        public async Task Gobiwan()
+        {
+            await Context.Channel.SendMessageAsync("https://media.giphy.com/media/Nx0rz3jtxtEre/giphy.gif");
+        }
+    }
+}
---
+++ AlbaBotDiscord/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Reflection;
+using System.Threading.Tasks;
+
+using System.IO;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace AlbaBotDiscord
+{
+    class Program
+    {
+        private DiscordSocketClient Client;
+        private CommandService Commands;
+
+        static void Main(string[] args)
+        {
+            new Program().MainAsync().GetAwaiter().GetResult();
+        }
+
+        private async Task MainAsync()
+        {
+            var Client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                LogLevel = LogSeverity.Debug
+            });
+
+            //Help without case
+            Commands = new CommandService(new CommandServiceConfig
+            {
+                CaseSensitiveCommands = true,
+                DefaultRunMode = RunMode.Async,
+                LogLevel = LogSeverity.Debug
+            });
+
+            Client.MessageReceived += Client_MessageReceived;
+            await Commands.AddModulesAsync(Assembly.GetEntryAssembly());
+
+            Client.Ready += Client_Ready;
+            Client.Log += Client_Log;
+
+            string Token = "";
+            using (var myStream = new FileStream((Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)).Replace(@"bin\Debug\netcoreapp2.1", @"Data\Token.conf"), FileMode.Open, FileAccess.Read))
+            using (var ReadToken = new StreamReader(myStream))
+            {
+                Token = ReadToken.ReadToEnd();
+            }
+            await Client.LoginAsync(TokenType.Bot, Token);
+            await Client.StartAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private async Task Client_Log(LogMessage message)
+        {
+            Console.WriteLine($"{DateTime.Now} at {message.Source}] {message.Message}");
+        }
+
+        private async Task Client_Ready()
+        {
+            await Client.SetGameAsync("Darksectoris", "https://www.sjustein.com/", StreamType.NotStreaming);
+        }
+
+        private async Task Client_MessageReceived(SocketMessage MessageParam)
+        {
+            var Message = MessageParam as SocketUserMessage;
+            var Context = new SocketCommandContext(Client, Message);
+            if (Context.Message == null || Context.Message.Content == "") return;
+            if (Context.User.IsBot) return;
+
+            int ArgPos = 0;
+            if (!(Message.HasStringPrefix("a!", ref ArgPos) || Message.HasMentionPrefix(Client.CurrentUser, ref ArgPos))) return;
+
+            var Result = await Commands.ExecuteAsync(Context, ArgPos);
+            if (!Result.IsSuccess)
+                Console.WriteLine($"{DateTime.Now} dans commandes] Désolé, je ne peux pas éxécuter vôtre commande. Texte: {Context.Message.Content} | Erreur: {Result.ErrorReason}");
+                await Context.Channel.SendMessageAsync($"Désolé, je ne peux pas éxécuter vôtre commande. Texte: {Context.Message.Content} | Erreur: {Result.ErrorReason}");
+        }
+    }
+}
