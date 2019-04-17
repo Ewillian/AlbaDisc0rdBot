4b2924f4dfa0a4a9185a5f08076e824a6850e030
Ewillian <guillaume.lecoq@ynov.com>
2019-04-17 16:04:56 +0200

    Added Random Image + Path Help

--- AlbaBotDiscord/Core/Commands/Help.cs
+++ AlbaBotDiscord/Core/Commands/Help.cs
@@ -11,19 +11,22 @@ namespace AlbaBotDiscord.Core.Commands
 {
     public class Help : ModuleBase<SocketCommandContext>
     {
+        public string msg;
         [Command("help"), Alias("h"), Summary("Liste des commandes disponible.")]
         public async Task help()
         {
             var comList = new List<string>();
-            comList.Add("?!Help ou ?!h || Affiche la liste des commandes.");
-            comList.Add("?!Hello ou ?!ht || Dit bonjour.");
-            comList.Add("?!BF ou ?!bf || Notifie joueurs de Battlefiel V.");
-            comList.Add("?!profile ou ?!pb || Description.");
+            comList.Add("?!Help ou ?!h | Affiche la liste des commandes.");
+            comList.Add("?!Hello ou ?!ht | Dit bonjour.");
+            comList.Add("?!BF ou ?!bf | Notifie joueurs de Battlefiel V.");
+            comList.Add("?!profile ou ?!pb | Petite introduction de ma personne.");
+            comList.Add("?!image ou ?!img | Affiche aléatoirement une image dans ma mémoire tampon.");
             await Context.Channel.SendMessageAsync("Liste des commandes :");
             foreach (String coms in comList)
             {
-                await Context.Channel.SendMessageAsync("`"+coms+"`");
+                msg += "\n "+ "`" + coms + "`"+ " \n";
             }
+            await Context.Channel.SendMessageAsync(msg);
         }
     }
 }
---
+++ AlbaBotDiscord/Core/Commands/Rand.cs
@@ -0,0 +1,33 @@
+﻿using Discord;
+using Discord.Commands;
+using System;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace AlbaBotDiscord.Core.Commands
+{
+    public class Rand : ModuleBase<SocketCommandContext>
+    {
+        public string path;
+        [Command("Image"), Summary("Send random image.")]
+        public async Task RandomImg()
+        {
+            Random rnd = new Random();
+            int rand = rnd.Next(1, 29);
+            
+
+            XDocument doc = XDocument.Load(AppDomain.CurrentDomain.BaseDirectory + "../../../Core/Data/ImgParameters.xml");
+            var keys = doc.Descendants("appSettings");
+
+            foreach (XElement el in doc.Root.Elements())
+            {
+               if(el.Attribute("key").Value == rand.ToString())
+                {
+                    path = el.Attribute("value").Value;
+                }
+            }
+            Console.WriteLine(path);
+            await Context.Channel.SendFileAsync("../../../Core/Data"+path);
+        }
+    }
+}
